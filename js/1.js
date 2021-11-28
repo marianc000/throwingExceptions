c5b7bff1e8a3b5b20fa4da73b8cf9e6302776b12
Marian <marian.caikovski@gmail.com>
2021-11-28 15:07:30 +0100

    first

--- js/1.js
+++ js/1.js
@@ -1,6 +1,5 @@
 // 1.js
-const err = new Error('MyMessage', { cause: 'MyCause' });
+const err = new Error('MyMessage', { cause: new RangeError("chainedMessage") });
 err.name = 'MyError';
 console.dir(err);
-console.dir(new RangeError());
 throw err;
